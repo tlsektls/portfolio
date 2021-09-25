e17c8157fa9b60ffa04a5efb3f5d2bbaa9f00da5
tlsektls <82145733+tlsektls@users.noreply.github.com>
2021-09-26 01:00:55 +0900

    Add files via upload

---
+++ works/narmalA/index.js
@@ -0,0 +1,57 @@
+$(document).ready(function() {
+  // 스크롤시 헤더 배경 생김
+  $(window).scroll(function(){
+    let user_scroll = $(window).scrollTop();
+    if(user_scroll > 50){
+      $('header').addClass('down');
+    } else if(user_scroll < 50){
+      $('header').removeClass('down');
+    }
+  });
+
+  // 모바일에서 gnb
+  if($(window).width() < 1024) {
+    $('.gnb_icon .menu_m').on('click',function(){
+      $('.gnb_wrap').slideToggle();
+    });
+  } else if($(window).width() >= 1024) {
+    $('.gnb_wrap').css({display:'block'});
+  }
+
+  // search아이콘과
+  // search를 누르면
+    $('.gnb_icon .search_m, .gnb .S').on('click',function(){
+      $('main, footer').css({display:'none'});
+      $('.search_layer').css({display:'block'});
+    });
+    $('.search_layer p').on('click',function(){
+      $('.search_layer').css({display:'none'});
+      $('main, footer').css({display:'block'});
+    });
+
+
+  const swiper = new Swiper(".back_container", {
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+      320: {
+        slidesPerView: 1,
+        spaceBetween: 40
+      },
+      768: {
+        slidesPerView: 2,
+        spaceBetween: 40
+      },
+      1024: {
+        slidesPerView: 3,
+        spaceBetween: 40
+      }
+    }
+  }); 
+
+  // aos
+  AOS.init();
+
+});
