db76a5945ffe35442662a7741bf8b6b923a0488c
tlsektls <82145733+tlsektls@users.noreply.github.com>
2021-10-04 20:51:45 +0900

    화면 사이즈 조절

--- works/work_gajagaja.js
+++ works/work_gajagaja.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
 
   // 화면이 작으면 안보이게
   $(window).resize(function () {
-    if($(window).width() <= 1024) {
+    if($(window).width() < 1440) {
       $("header, main").css({display: 'none'});
       $(".inform").css({display: 'block'});
     } else {
