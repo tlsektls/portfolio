53d254592d64eeae1b9c511761b4901fad5837b9
tlsektls <82145733+tlsektls@users.noreply.github.com>
2021-10-04 20:41:39 +0900

    화면 작아졌을때 사이트 안보이게

---
+++ works/work_gajagaja.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+
+  // 화면이 작으면 안보이게
+  $(window).resize(function () {
+    if($(window).width() <= 1024) {
+      $("header, main").css({display: 'none'});
+      $(".inform").css({display: 'block'});
+    } else {
+      $("header, main").css({display: 'block'});
+      $(".inform").css({display: 'none'});
+    }
+  });
+
+
+});
